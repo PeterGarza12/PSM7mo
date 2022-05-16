727fd44cf37917772fc876abb974cd9908f43498
PeterGarza12 <pedro.garzagnz@uanl.edu.mx>
2022-05-16 01:31:29 -0500

    CreateChapterActivity.kt

---
+++ app/src/main/java/com/psm/horrorg/Utilities/CreateChapterActivity.kt
@@ -0,0 +1,23 @@
+package com.psm.horrorg
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_createbook.*
+
+class CreateChapterActivity: AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addchapter)
+
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
+}
