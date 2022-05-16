5ab3406255b1b00f91655565347596f6a9daf40b
1es1ie0 <leslie-rosas@hotmail.com>
2022-05-16 01:16:26 -0500

    Validacion correo y contrasena

--- app/src/main/java/com/psm/horrorg/RegisterActivity.kt
+++ app/src/main/java/com/psm/horrorg/RegisterActivity.kt
@@ -5,14 +5,17 @@ import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.core.text.set
+import androidx.core.util.PatternsCompat
 import com.psm.horrorg.DatePicker.DatePickerFragment
 import com.psm.horrorg.Db.DbHelper
 import com.psm.horrorg.Db.dbUsers
 import kotlinx.android.synthetic.main.activity_register.*
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity(),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,10 +48,24 @@ class RegisterActivity : AppCompatActivity(),View.OnClickListener {
     }
 
     private fun showInfo(){
+        val passwordRegex =Pattern.compile(
+            "^"+"(?=.*[0-9])" +         //al menos un digito
+                    "(?=.*[a-z])" +        //al menos una minuscula
+                    "(?=.*[A-Z])" +        //al menos una mayuscula
+                    "(?=\\S+$)" +           //sin espacios
+                    ".{8,}" +               //al menos 4 caracteres
+                    "$"
+        )
         if(this.input_username.text.toString() =="" && this.input_password.text.toString()=="" && this.input_password2.text.toString()==""&&this.input_password2.text.toString()=="" &&this.editTextDate.text.toString()==""){
             Toast.makeText(this,"Favor de llenar los campos",Toast.LENGTH_LONG).show();
         }else if(this.input_password.text.toString()!=this.input_password2.text.toString()){
             Toast.makeText(this,"Las contraseñas tienen que ser iguales",Toast.LENGTH_LONG).show();
+        }else if(!PatternsCompat.EMAIL_ADDRESS.matcher(this.input_correo.text.toString()).matches()){
+            Toast.makeText(this,"El Formato del correo electronico es invalido",Toast.LENGTH_LONG).show();
+
+        }else if (!passwordRegex.matcher(this.input_password.text.toString()).matches()){
+
+            Toast.makeText(this,"El formato de la contraseña es invalido",Toast.LENGTH_LONG).show();
         }else{
 
             var Dbusers = dbUsers(this@RegisterActivity)
@@ -66,13 +83,11 @@ class RegisterActivity : AppCompatActivity(),View.OnClickListener {
             }
             else{
                 Toast.makeText(this,"Error al crear el usuario",Toast.LENGTH_LONG).show();
-                clean()
+
             }
 
         }
     }
-    private fun clean(){
 
-    }
 
 }
