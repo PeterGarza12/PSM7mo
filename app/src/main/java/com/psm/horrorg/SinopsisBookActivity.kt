0a6dd181dd7399a59daf97569b38f99a1178cd3a
1es1ie0 <leslie-rosas@hotmail.com>
2022-05-19 15:23:02 -0500

    Genera pdf en sinopsis

--- app/src/main/java/com/psm/horrorg/SinopsisBookActivity.kt
+++ app/src/main/java/com/psm/horrorg/SinopsisBookActivity.kt
@@ -1,18 +1,34 @@
 package com.psm.horrorg
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.graphics.pdf.PdfDocument
 import android.os.Bundle
+import android.os.Environment
+import android.text.TextPaint
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.psm.horrorg.Model.Libro
-import com.psm.horrorg.Model.Libros
 import com.psm.horrorg.Model.Usuario
+import kotlinx.android.synthetic.main.activity_sinopsis_book.*
+import java.io.File
+import java.io.FileOutputStream
+
 
 class SinopsisBookActivity : AppCompatActivity() {
+    val background = findViewById<ImageView>(R.id.iv_sinopsis_background)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sinopsis_book)
@@ -25,7 +41,7 @@ class SinopsisBookActivity : AppCompatActivity() {
         val tvTitle = findViewById<TextView>(R.id.txt_titulo)
         val tvCategory = findViewById<TextView>(R.id.txt_Categoria)
         val tvDescription = findViewById<TextView>(R.id.txt_Descripcion)
-        val background = findViewById<ImageView>(R.id.iv_sinopsis_background)
+
 
         /*tvTitle.text = miLibro.strTitle
         tvCategory.text = miLibro.genre.toString()
@@ -36,7 +52,7 @@ class SinopsisBookActivity : AppCompatActivity() {
         tvCategory.text = Libro.getGenre()
         tvDescription.text = Libro.getDescription()
         background.setImageBitmap(Libro.getimgArray())
-
+        background.setTag(Libro.getimgArray())
 
 
         val btnRead = findViewById<Button>(R.id.btn_Read)
@@ -52,6 +68,12 @@ class SinopsisBookActivity : AppCompatActivity() {
             btnRead.visibility = View.VISIBLE
         }
 
+        if(sheckPermission()){
+            Toast.makeText(this,"Permiso Aceptado", Toast.LENGTH_LONG)
+        }else{
+            requestPermissions()
+        }
+
         btnRead.setOnClickListener {
             val intent = Intent(this, ReadActivity::class.java)
             startActivity(intent)
@@ -59,6 +81,80 @@ class SinopsisBookActivity : AppCompatActivity() {
         btnCreate.setOnClickListener{
             val intent = Intent(this, CreateChapterActivity::class.java)
             startActivity(intent)
+        }
+        txt_Download.setOnClickListener(View.OnClickListener {
+            generarPdf()
+        })
+    }
+    fun generarPdf(){
+        //inicializas la variable para el pdf
+        var pdfDocument = PdfDocument()
+        // inicializas la variable para dibujar la informacion en el pdf
+        var paint = Paint()
+        var titulo =TextPaint()
+        var categoria= TextPaint()
+        var descripcion= TextPaint()
+
+        //configuraciones , caracteristicas que queremos dentro del builder
+        var  paginaInfo = PdfDocument.PageInfo.Builder(815,1054,1).create()
+        var pagina1 = pdfDocument.startPage(paginaInfo)
+        var canvas = pagina1.canvas
+
+        val drawableId: Int = background.getTag().toString().toInt()
+
+
+        var bitmap= BitmapFactory.decodeResource(resources,drawableId)
+        var bitmapEscala= Bitmap.createScaledBitmap(bitmap,80,80,false)
+        canvas.drawBitmap(bitmapEscala,368f,20f,paint)
+
+        titulo.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD)) //titulo en negritas
+        titulo.textSize=20f
+        canvas.drawText(Libro.getTitle(),10f,150f,titulo)
+        descripcion.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL))
+        descripcion.textSize=14f
+
+        var arrayDescripcion = Libro.getDescription().split(".")
+        var y = 200f // la alttura por la que deseamos [intar para no sobreescribir nuestro texto
+
+        for(item in arrayDescripcion){
+            canvas.drawText(item,10f,150f,descripcion)
+            y+= 15
+        }
+        pdfDocument.finishPage(pagina1)
+
+        val file =File(Environment.getExternalStorageDirectory(),Libro.getDescription()+".pdf")
+        try{
+            pdfDocument.writeTo(FileOutputStream(file))
+            Toast.makeText(this,"Se descargo el pdf", Toast.LENGTH_LONG).show()
+        }
+        catch (e: Exception){
+            e.printStackTrace()
+        }
+        pdfDocument.close()
+
+    }
+    private fun  sheckPermission():Boolean{
+        val permission1 = ContextCompat.checkSelfPermission(applicationContext,WRITE_EXTERNAL_STORAGE)
+        val permission2 = ContextCompat.checkSelfPermission(applicationContext,READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED
+    }
+    private fun requestPermissions(){
+        ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE,READ_EXTERNAL_STORAGE ),200)
+    }
+    override fun onRequestPermisionResult(requestCode: Int,permissions:Array<String>,grantResul:IntArray){
+        if(requestCode==200){
+            if(grantResul.size>0){
+                val writeStorage = grantResul[0]== PackageManager.PERMISSION_GRANTED
+                val readStorage = grantResul[1]== PackageManager.PERMISSION_GRANTED
+
+                if(writeStorage && readStorage){
+                    Toast.makeText(this, "Permiso concedido", Toast.LENGTH_LONG)
+                }else{
+                    Toast.makeText(this, "Permiso rechazado", Toast.LENGTH_LONG)
+                    finish()
+                }
+
+            }
         }
     }
 }
