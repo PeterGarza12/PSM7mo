98eac43355847959b52b39a87bbe36e7050c1ddb
PeterGarza12 <pedro.garzagnz@uanl.edu.mx>
2022-05-19 23:49:29 -0500

    Muestra al usuario en el header

--- app/src/main/java/com/psm/horrorg/DrawerActivity.kt
+++ app/src/main/java/com/psm/horrorg/DrawerActivity.kt
@@ -4,6 +4,8 @@ import android.app.Notification
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -15,11 +17,14 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import com.psm.horrorg.Fragments.HomeFragment
 import com.psm.horrorg.Fragments.ProfileUser
+import com.psm.horrorg.Model.Usuario
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.drawer_header.*
 
 class DrawerActivity : AppCompatActivity() {
 
     private lateinit var nombreUsuario: String
+    private lateinit var picdrawer: ImageView
 
     fun cambiarFragmento(fragmentoNuevo: Fragment, tag: String){
 
@@ -37,15 +42,25 @@ class DrawerActivity : AppCompatActivity() {
         setContentView(R.layout.drawer_layout)
         val extras = intent.extras
 
-        val miNav = findViewById<NavigationView>(R.id.nav)
+        val miNav = findViewById<NavigationView>(R.id.nav_drawer_header)
         val miDrawer = findViewById<DrawerLayout>(R.id.drawer)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
 
 
         setSupportActionBar(toolbar)
         val toggle = ActionBarDrawerToggle(this, miDrawer, toolbar, R.string.app_name, R.string.app_name)
         miDrawer.addDrawerListener(toggle)
         toggle.syncState()
+
+
+        val header = miNav.getHeaderView(0)
+        picdrawer = header.findViewById(R.id.iv_drawer_perfil)
+        val namedrawer = header.findViewById<TextView>(R.id.tv_drawer_name)
+
+        namedrawer.text = Usuario.getUsername()
+        picdrawer.setImageBitmap(Usuario.getImage())
+
 
 
         //nombreUsuario = intent.getStringExtra("username") ?: "sin_nombre"
